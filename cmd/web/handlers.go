b3719082cf20c45f41a1e55f9ff12cd8026da9a4
Cipto Hadi <ciptohadi79@gmail.com>
2023-12-21 08:30:00 +0700

    finish create terminal & charge-once feature - no db

---
+++ cmd/web/handlers.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"myapp/internal/models"
+	"net/http"
+)
+
+func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "terminal", nil, "stripe-js"); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+
+	// read posted data
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["pi"] = paymentIntent
+	data["pm"] = paymentMethod
+	data["pa"] = paymentAmount
+	data["pc"] = paymentCurrency
+
+	if err := app.renderTemplate(
+		w, r, "succeeded",
+		&templateData{
+			Data: data,
+		}); err != nil {
+		app.errorLog.Println(err)
+	}
+
+}
+
+// ChargeOnce displays the page to buy one widget
+func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
+	// id := chi.URLParam(r, "id")
+	// widgetID, _ := strconv.Atoi(id)
+
+	widget := models.Widget{
+		ID:             1,
+		Name:           "Custom Widget",
+		Description:    "A very nice widget",
+		InventoryLevel: 10,
+		Price:          1000,
+	}
+	// if err != nil {
+	// 	app.errorLog.Println(err)
+	// 	return
+	// }
+
+	data := make(map[string]interface{})
+	data["widget"] = widget
+
+	if err := app.renderTemplate(w, r, "buy-once", &templateData{Data: data}, "stripe-js"); err != nil {
+		app.errorLog.Println(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,30 @@
+module myapp
+
+go 1.21
+
+require (
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20231113091146-cef4b05350c8
+	github.com/alexedwards/scs/v2 v2.7.0
+	github.com/bwmarrin/go-alone v0.0.0-20190806015146-742bb55d1631
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1
+	github.com/go-playground/validator/v10 v10.16.0
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/gorilla/websocket v1.5.1
+	github.com/phpdave11/gofpdf v1.4.2
+	github.com/stripe/stripe-go/v72 v72.122.0
+	github.com/xhit/go-simple-mail/v2 v2.16.0
+	golang.org/x/crypto v0.17.0
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-test/deep v1.1.0 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/phpdave11/gofpdi v1.0.13 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
